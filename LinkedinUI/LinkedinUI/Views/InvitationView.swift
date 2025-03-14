29539361ba1c80d147c5870958afd3145cde8c12
GauravSemwal46 <gauravsemwal6@gmail.com>
2025-03-14 09:54:27 +0530

    create UI for the invitation view

---
+++ LinkedinUI/LinkedinUI/Views/InvitationView.swift
@@ -0,0 +1,46 @@
+//
+//  InvitationView.swift
+//  LinkedinUI
+//
+//  Created by Gaurav Semwal on 14/03/25.
+//
+
+import SwiftUI
+
+let sampleData = NetworkModel(id: 1, name: "Gaurav Semwal", position: "IT Analyst", mutual: 12, image: "01")
+
+struct InvitationView: View {
+    var data: NetworkModel
+    var body: some View {
+        HStack(alignment: .center, spacing: 10) {
+            Image(data.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .clipShape(Circle())
+                .frame(width:70, height: 70, alignment: .leading)
+            VStack(alignment: .leading) {
+                Text(data.name)
+                    .font(.body)
+                Text(data.position)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                Text("\(data.mutual) mutual connections")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+            }.frame(width: 150, height: 20, alignment: .leading)
+            HStack {
+                Image(systemName: "multiply.circle")
+                    .font(.system(size: 35))
+                    .foregroundColor(.gray)
+                Image(systemName: "checkmark.circle")
+                    .font(.system(size: 35))
+                    .foregroundColor(.blue.opacity(0.8))
+            }.padding(.horizontal)
+        }.frame(width: .infinity, height: .infinity)
+            .padding(.horizontal)
+    }
+}
+
+#Preview {
+    InvitationView(data: sampleData)
+}
