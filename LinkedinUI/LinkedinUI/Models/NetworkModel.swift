2fa249fa9462bd8c58d5301c838ec0693d48428e
GauravSemwal46 <gauravsemwal6@gmail.com>
2025-03-14 09:43:47 +0530

    create UI for search bar and add models in the project

---
+++ LinkedinUI/LinkedinUI/Models/NetworkModel.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkModel.swift
+//  LinkedinUI
+//
+//  Created by Gaurav Semwal on 14/03/25.
+//
+
+import Foundation
+
+struct NetworkModel {
+    var id: Int
+    var name: String
+    var position: String
+    var mutual: String
+    var image: String
+}
---
+++ LinkedinUI/LinkedinUI/Models/PostModel.swift
@@ -0,0 +1,16 @@
+//
+//  PostModel.swift
+//  LinkedinUI
+//
+//  Created by Gaurav Semwal on 14/03/25.
+//
+
+import Foundation
+
+struct PostData {
+    var id: Int
+    var image: String
+    var title: String
+    var followers: Int
+    var profileImage: String
+}
