42406d5f17a89f87fa574efc6043838b1bc8561a
GauravSemwal46 <gauravsemwal6@gmail.com>
2025-03-14 09:09:56 +0530

    initial commit

---
+++ LinkedinUI/LinkedinUI/LinkedinUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedinUIApp.swift
+//  LinkedinUI
+//
+//  Created by Gaurav Semwal on 14/03/25.
+//
+
+import SwiftUI
+
+@main
+struct LinkedinUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
